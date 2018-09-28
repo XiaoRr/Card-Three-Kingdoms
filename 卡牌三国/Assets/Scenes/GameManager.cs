dea3b2389d40205483c47fa989edfa3c20319a53
小R <xufujian777@gmail.com>
2018-09-29 00:03:42 +0800

    图片的重新组织 继续深化卡牌类

--- 卡牌三国/Assets/Scenes/GameManager.cs
+++ 卡牌三国/Assets/Scenes/GameManager.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-   
+    public RealCard[] enemy, our;   //敌我卡组
+
+    private int turn = 1;   //当前回合
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
---
+++ 卡牌三国/Assets/Scenes/Global.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using UnityEngine;
+
+public class Global
+{
+    public List<Card> cards;    //所有卡片的数据库
+
+    public void Load()
+    {
+        //读取所有卡片
+        cards = new List<Card>();
+        //路径  
+        string fullPath = "Assets/Resources/Cards/";  //路径
+
+        //获取指定路径下面的所有资源文件  
+        if (Directory.Exists(fullPath))
+        {
+            DirectoryInfo direction = new DirectoryInfo(fullPath);
+            FileInfo[] files = direction.GetFiles("*", SearchOption.AllDirectories);
+
+            Debug.Log(files.Length);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].Name.EndsWith(".meta"))
+                {
+                    continue;
+                }
+                cards.Add(new Card(files[i].Name));
+            }
+        }
+    }
+
+}
