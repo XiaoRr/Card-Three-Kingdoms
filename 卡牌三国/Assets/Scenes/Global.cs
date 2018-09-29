c67880130d2f437f93a7e46ebe7d1f67869c515c
小R <xufujian777@gmail.com>
2018-09-30 01:02:23 +0800

    完善场景 继续组织图片 开始编写游戏逻辑

--- 卡牌三国/Assets/Scenes/Global.cs
+++ 卡牌三国/Assets/Scenes/Global.cs
@@ -5,14 +5,14 @@ using System.Text;
 using System.IO;
 using UnityEngine;
 
-public class Global
+public static class Global
 {
-    public List<Card> cards;    //所有卡片的数据库
-
-    public void Load()
+    //读取所有卡片
+    public static Dictionary<string, Card> Load()
     {
-        //读取所有卡片
-        cards = new List<Card>();
+
+        List<Card> cards = new List<Card>();
+        Dictionary<string,Card> dictionary = new Dictionary<string, Card>();
         //路径  
         string fullPath = "Assets/Resources/Cards/";  //路径
 
@@ -30,9 +30,36 @@ public class Global
                 {
                     continue;
                 }
-                cards.Add(new Card(files[i].Name));
+                if ('0' <= files[i].Name[0] && files[i].Name[0] <= '9' || 'a' <= files[i].Name[0] && files[i].Name[0] <= 'z')      //未完成重命名
+                {
+                    continue;
+                }
+                Card tmp;
+                cards.Add(tmp = new Card(files[i].Name));
+                if (dictionary.ContainsKey(tmp.name))   //出现重复名字是因为大部分卡有两种品质
+                {
+                    if(dictionary[tmp.name].rare < tmp.rare)    //如果字典里的品质较低
+                    {
+                        //Debug.Log("出现了不同品质的卡" + tmp.name);
+                        dictionary[tmp.name + 'L'] = dictionary[tmp.name];  //把品质低的key增加一个L字样
+                        dictionary[tmp.name] = tmp;
+                    }
+                    else if(dictionary[tmp.name].rare < tmp.rare)
+                    {
+                        //Debug.Log("出现了不同品质的卡" + tmp.name);
+                        dictionary[tmp.name + 'L'] = tmp;    //把品质低的key增加一个L字样
+                    }
+                    else
+                    {
+                        Debug.Log("出现了重复卡" + tmp.name);
+                    }
+                }
+                dictionary[tmp.name] = tmp;    //添加到map索引
+                //cards.Last<Card>();
             }
         }
+
+        return dictionary;
     }
 
 }
