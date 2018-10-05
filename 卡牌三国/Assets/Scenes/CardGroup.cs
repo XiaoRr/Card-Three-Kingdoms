105d328220a518c5da58723c67f47c8ed5a736f1
小R <xufujian777@gmail.com>
2018-10-06 02:04:04 +0800

    新dlc 群雄势力 闪亮登场

---
+++ 卡牌三国/Assets/Scenes/CardGroup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+//卡片容器类的父类，抽象
+public abstract class CardGroup:MonoBehaviour
+{
+    private List<RealCard> cards;    //一个有序的抽象数组
+
+    public GameObject owner;    //游戏中对应的载体
+    //把该类中的指定对象送向另一个CardGroup
+    public void sendTo(RealCard tar,CardGroup otherGroup)
+    {
+        if (!cards.Contains(tar))
+        {
+            Debug.Log("未找到对应卡片" + tar.name);
+            return;
+        }
+        otherGroup.add(tar);
+        cards.Remove(tar);
+    }
+
+    //另一个可能的重载
+    public void sendTo(int tarId,CardGroup otherGroup)
+    {
+        if (cards.Count <= tarId)
+        {
+            Debug.Log("未找到对应卡片" + cards[tarId].name);
+            return;
+        }
+        otherGroup.add(cards[tarId]);
+        cards.RemoveAt(tarId);
+        //对两个容器进行刷新
+        this.refresh();
+        otherGroup.refresh();
+    }
+
+
+    public void add(RealCard card)
+    {
+        cards.Add(card);
+    }
+
+    //刷新事件，容器内容变化时 需要重新显示容器 例如手牌由2张变为3张时的重绘
+    public abstract void refresh();
+}
