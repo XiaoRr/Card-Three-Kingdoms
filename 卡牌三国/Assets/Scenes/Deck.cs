2f89b004ec01446314928eded30801c9c168e7af
小R <xufujian777@gmail.com>
2018-10-13 03:16:16 +0800

    排版修改 开始进行游戏场景中的物件与脚本的映射 修复部分错误卡牌名称 该版本依然无法运行

--- 卡牌三国/Assets/Scenes/Deck.cs
+++ 卡牌三国/Assets/Scenes/Deck.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
 
 class Deck:CardGroup
 {
 
+
+    public new void Add(RealCard card)
+    {
+        base.Add(card);
+        //card.transform.parent = this.owner.transform;
+
+    }
     public override void refresh()
     {
+        //if()
+        //throw new NotImplementedException();
+    }
 
-        throw new NotImplementedException();
+    //卡组不需要显示卡片 只需要显示卡组里有么有卡
+    void Update()
+    {
+        if (cards.Any())
+        {
+            this.GetComponent<CanvasGroup>().alpha = 1;
+        
+        }
+        else
+        {
+            this.GetComponent<CanvasGroup>().alpha = 0;
+        }
     }
 }
 
---
+++ 卡牌三国/Assets/Scenes/Hand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Hand : CardGroup
+{
+    public override void refresh()
+    {
+        //throw new NotImplementedException();
+    }
+
+    public new void Add(RealCard card)
+    {
+        base.Add(card);
+        card.transform.parent = this.owner.transform;   //设置卡牌父节点为手牌
+    }
+}
+
