1b4146c7130062f6c2b0da7cef66b65eaec8e539
小R <xufujian777@gmail.com>
2018-10-19 18:41:21 +0800

    token类设计完成 lay了

---
+++ 卡牌三国/Assets/Scripts/UIControll/AutoUIImg.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//控制图标的大小 图标大小应该为卡牌宽度的三分之一
+//缩放而不是直接改变大小，是因为该图标内还有子物体
+public class AutoUIImg : MonoBehaviour {
+
+    public RectTransform Card;  //寻找一个卡牌作为参照物
+	// Use this for initialization
+	void Start () {
+        var size = Card.sizeDelta.x / 3;
+        this.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(size,size);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
